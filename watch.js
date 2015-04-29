5aa7f11cdbb29d9cf0db5ff7b6f711f43381187a
unknown <Viktor_Abramov@EPUALVIW0809.kyiv.epam.com>
2015-04-29 18:41:25 +0300

    added minified files

--- watch.js
+++ watch.js
@@ -8,7 +8,7 @@
 	};
 
 	var _getClock = function() {
-		$.ajax({url:'clock.html'}).done(function(data) {
+		$.ajax({url:'clock.min.html'}).done(function(data) {
 			$('.watch-container').html(data);
 		});
 		_initClock();
@@ -18,8 +18,8 @@
 		setInterval(function() {
 			if (!_digits) _digits = $('.digits div');
 			_initDayMarker();
-			_initTimeCounter();
-		}, 1000);
+            _startTimer();
+		}, 1e3);
 	};
 
 	var _initDayMarker = function() {
@@ -29,38 +29,20 @@
 		$('.weekdays span:contains(' + day + ')').addClass('active');
 	};
 
-	var _initTimeCounter = function() {
-        _startHoursCounter();
-        _startMinutesCounter();
-        _startSecondsCounter();
-	};
-
-	var _startHoursCounter = function() {
-		var hours = [_digits[0], _digits[1]];
-		var time = '' + (new Date).getHours();
-        if (time.length < 2) time = '0' + time;
-		$.each(hours, function(index, hour) {
-			$(hour).removeClass().addClass(_self.digits[+time[index]]);
-		});
-	};
-
-    var _startMinutesCounter = function() {
-        var minutes = [_digits[3], _digits[4]];
-        var time = '' + (new Date).getMinutes();
-        if (time.length < 2) time = '0' + time;
-        $.each(minutes, function(index, minute) {
-            $(minute).removeClass().addClass(_self.digits[+time[index]]);
+	var _startTimer = function() {
+        [
+            { digits: [_digits[0], _digits[1]], getter: 'getHours' },
+            { digits: [_digits[3], _digits[4]], getter: 'getMinutes' },
+            { digits: [_digits[6], _digits[7]], getter: 'getSeconds' }
+        ].forEach(function(item) {
+            var time = '' + (new Date)[item.getter]();
+            if (time.length < 2) time = '0' + time;
+            item.digits.forEach(function(digit, index) {
+                $(digit).removeClass().addClass(_self.digits[+time[index]])
+            })
         });
-    };
 
-    var _startSecondsCounter = function() {
-        var seconds = [_digits[6], _digits[7]];
-        var time = '' + (new Date).getSeconds();
-        if (time.length < 2) time = '0' + time;
-        $.each(seconds, function(index, second) {
-            $(second).removeClass().addClass(_self.digits[+time[index]]);
-        });
-    };
+	};
 
 	var _digits = null;
 
